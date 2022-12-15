7f270f8467701e8b7d888eb4dab8a83fff165457
kaua.wadson <kaua.wadson@ofm.com.br>
2022-12-15 13:30:52 -0300

    Fix: iniciando javascript

--- javaScript/index.js
+++ javaScript/index.js
@@ -23,3 +23,84 @@ function clickmenu() {
 function openMenu() {
 
 }
+
+function functionIrPgCadastra() {
+  window.location.href = '../pages/telaDePergunta.html';
+}
+
+function functionUsuario(funcao) {
+  let atributoEdit = document.querySelectorAll('.box_tela');
+  let jogador = document.getElementById('jogador');
+  let colaborador = document.getElementById('colaborador');
+  let titlePergunta = document.getElementById('title_pergunta');
+  if (funcao === 'jogador') {
+    atributoEdit.forEach(element => {
+      element.style.display = 'none';
+    });
+    titlePergunta.style.display = 'none';
+    jogador.style.display = 'block';
+  } else if (funcao === 'colaborador') {
+    atributoEdit.forEach(element => {
+      element.style.display = 'none';
+    });
+    titlePergunta.style.display = 'none';
+    colaborador.style.display = 'block';
+  }
+}
+
+
+function functionCadastra() {
+
+  let msg = document.getElementById('message');
+  let jogador = document.getElementById('jogador');
+  let colaborador = document.getElementById('colaborador');
+  if (jogador.style.display != 'none') {
+    console.log('Entrou aqui')
+    let nome = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let senha1 = document.getElementById('password').value;
+    let senha2 = document.getElementById('password2').value;
+    let termos = document.getElementById('termos_uso');
+    if (!termos.checked) {
+      msg.style.display = 'block';
+      msg.innerHTML = 'Para continuar precisa aceitar os termos.';
+    } else {
+      if (senha1 !== senha2) {
+        msg.style.display = 'block';
+        msg.innerHTML = 'Senhas tem que ser iguais.';
+      }
+      Storage.setItem('nome', nome);
+      Storage.setItem('email', email);
+      Storage.setItem('senha', senha1);
+      Storage.setItem('termo', termos);
+      Storage.setItem('funcao', 'jogador');
+
+    }
+  } else if (colaborador.style.display != 'none') {
+    console.log('Entrou aqui')
+    let nome = document.getElementById('name_c').value;
+    let email = document.getElementById('email_c').value;
+    let senha1 = document.getElementById('password_c').value;
+    let senha2 = document.getElementById('password2_c').value;
+    let cnpj = document.getElementById('cnpj_c').value;
+    let termos = document.getElementById('termos_uso_c');
+
+    if (!termos.checked) {
+      msg.style.display = 'block';
+      msg.innerHTML = 'Para continuar precisa aceitar os termos.';
+    } else {
+      if (senha1 !== senha2) {
+        msg.style.display = 'block';
+        msg.innerHTML = 'Senhas tem que ser iguais.';
+      }
+      Storage.setItem('nome', nome);
+      Storage.setItem('email', email);
+      Storage.setItem('senha', senha1);
+      Storage.setItem('cnpj', cnpj);
+      Storage.setItem('termo', termos);
+      Storage.setItem('funcao', 'jogador');
+      
+    }
+  }
+
+}
